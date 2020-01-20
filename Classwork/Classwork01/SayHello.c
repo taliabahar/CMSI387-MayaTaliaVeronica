68e249b2e8b2eee5c69fa29c18471123e73bf8e0
taliabahar <taliabahar@yahoo.com>
2020-01-20 14:12:54 -0800

    Update SayHello.c

--- Classwork/Classwork01/SayHello.c
+++ Classwork/Classwork01/SayHello.c
@@ -1,10 +1,18 @@
+ /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  SayHello.c
+ *  Purpose       :  Provides a simple method to retrieve and print out user given data. 
+ *  Author        :  Talia Bahar, Maya Pegler-Gordon, Veronica Backer Peral 
+ *  Date          :  2020-01-20
+ *  Description   :  This program prompts the user for their name using the printf() and gets() functions
+ *                   and then prints "Hello" followed by the user's entered name.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (int argc, char * argv[] ) {
-	char input[25];
-	printf( "Enter a name: ");
-	gets(input);
-	printf ( "\n\n Hello, %s!\n\n", input);
-	exit(0);
+  char input[25];
+  printf( "Enter a name: ");
+  gets(input);
+  printf ( "\n\n Hello, %s!\n\n", input);
+  exit(0);
 }
